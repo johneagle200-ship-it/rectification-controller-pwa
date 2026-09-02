1123880153e3f9af134241adac42dafba1d55296
johneagle200-ship-it <johneagle200@gmail.com>
2026-09-02 15:40:36 +0300

    Refactor service worker to use constants for cache

--- sw.js
+++ sw.js
@@ -1,9 +1,22 @@
+const CACHE_NAME = 'pwa-rect-v2';
+const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('pwa-cache-v1').then((cache) => {
-      return cache.addAll(['./', './index.html', './app.js', './manifest.json']);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => 
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
